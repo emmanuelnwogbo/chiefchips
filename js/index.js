4a835221721eff900fd4785d87cd8553344a0515
Emmanuel Nwogbo <nerdyemmanuel@gmail.com>
2020-07-11 13:54:44 +0100

    jkrgfywerh

--- js/index.js
+++ js/index.js
@@ -1,4 +1,10 @@
 const slides = Array.from(document.querySelectorAll(".slider__slide"));
+const dropdownItems = Array.from(
+  document.querySelectorAll(".header__dropdown--item")
+);
+const dropdownCheck = document.getElementById("header-check");
+const headerLabel = document.getElementById("header-label");
+const variantNavigatiors = document.querySelectorAll(".variants__navi");
 
 let slideTracker = 0;
 slides[slideTracker].style.opacity = 1;
@@ -18,3 +24,24 @@ setInterval(function () {
     slides[slideTracker].style.opacity = 1;
   }
 }, 5000);
+
+headerLabel.addEventListener("click", function () {
+  if (!dropdownCheck.checked) {
+    setTimeout(function () {
+      dropdownItems.forEach((item) => {
+        item.style.opacity = 1;
+      });
+    }, 100);
+
+    document.getElementsByTagName("body")[0].style.overflow = "hidden";
+  } else {
+    dropdownItems.forEach((item) => {
+      item.style.opacity = 0;
+      document.getElementsByTagName("body")[0].style.overflow = "scroll";
+    });
+  }
+});
+
+variantNavigatiors.addEventListener('click', function () {
+  console.log('yooo')
+});
