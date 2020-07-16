1e0767d59623571414104e009e8e36e3d8366015
Emmanuel Nwogbo <nerdyemmanuel@gmail.com>
2020-07-16 04:22:26 +0100

    hgkbfjr

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,53 @@
 import "../sass/main.scss";
 
 import swipe from "pure-swipe-js";
+import loadGoogleMapsApi from "load-google-maps-api";
+
+import "./variants";
+
+const mapItems = Array.from(
+  document.querySelectorAll(".locations__boxes--item")
+);
+
+mapItems.forEach((item) => {
+  item.addEventListener("click", function () {
+    const map = document.getElementById("map-content");
+
+    map.style.opacity = "1";
+    map.style.zIndex = "10";
+
+    document.getElementById("map-close").addEventListener("click", (event) => {
+      if (event.target.id === "map-close") {
+        map.style.opacity = "0";
+        map.style.zIndex = "-1";
+      }
+    });
+  });
+});
+
+class Map {
+  static loadGoogleMapsApi() {
+    return loadGoogleMapsApi({
+      key: "AIzaSyDFdPqz2fYoM39ld7gTxES1zvTuJYyc9S4",
+    });
+  }
+  static createMap(googleMaps, mapElement) {
+    return new googleMaps.Map(mapElement, {
+      center: { lat: 6.7148901, lng: 3.2231429 },
+      zoom: 14,
+    });
+  }
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+  let mapElement = document.getElementById("map");
+
+  //document.querySelectorAll('.')
+
+  Map.loadGoogleMapsApi().then(function (googleMaps) {
+    Map.createMap(googleMaps, mapElement);
+  });
+});
 
 const slides = Array.from(document.querySelectorAll(".slider__slide"));
 const dropdownItems = Array.from(
@@ -17,75 +64,85 @@ const variantSlides = Array.from(
 const variantSlide = document.getElementById("variants-slide");
 const mobileVariantCheck = document.getElementById("mobile-variant-check");
 
+const variantBtns = Array.from(document.querySelectorAll(".variants-button"));
+
 console.log(variantSlide);
 console.log(variantSlides);
 
-variantSlides[0].addEventListener("swipe.progress", function (e) {
-  console.log("swiped begin", e.detail.direction, e);
-  const direction = e.detail.direction;
-  console.log(e.detail.x, e.detail.y, "dir");
+variantSlides[0] !== undefined
+  ? variantSlides[0].addEventListener("swipe.progress", function (e) {
+      console.log("swiped begin", e.detail.direction, e);
+      const direction = e.detail.direction;
+      console.log(e.detail.x, e.detail.y, "dir");
 
-  if (direction === null) {
-    return;
-  }
+      if (direction === null) {
+        return;
+      }
 
-  if (direction % 2 === 0) {
-    console.log("right");
-    variantSlides[0].style.transform = "translateX(0)";
-    variantSlides[1].style.transform = "translateX(0)";
-    variantSlides[2].style.transform = "translateX(0)";
-  } else {
-    console.log("left");
-    variantSlides[0].style.transform = "translateX(-103%)";
-    variantSlides[1].style.transform = "translateX(-100%)";
-    variantSlides[2].style.transform = "translateX(-97%)";
-    return;
-  }
-});
+      if (direction % 2 === 0) {
+        console.log("right");
+        variantSlides[0].style.transform = "translateX(0)";
+        variantSlides[1].style.transform = "translateX(0)";
+        variantSlides[2].style.transform = "translateX(0)";
+      } else {
+        console.log("left");
+        variantSlides[0].style.transform = "translateX(-103%)";
+        variantSlides[1].style.transform = "translateX(-100%)";
+        variantSlides[2].style.transform = "translateX(-97%)";
+        return;
+      }
+    })
+  : "";
 
-variantSlides[1].addEventListener("swipe.progress", function (e) {
-  console.log("swiped begin", e.detail.direction, e);
-  const direction = e.detail.direction;
-  console.log(e.detail.x, e.detail.y, "dir");
+variantSlides[1] !== undefined
+  ? variantSlides[1].addEventListener("swipe.progress", function (e) {
+      console.log("swiped begin", e.detail.direction, e);
+      const direction = e.detail.direction;
+      console.log(e.detail.x, e.detail.y, "dir");
 
-  if (direction === null) {
-    return;
-  }
+      if (direction === null) {
+        return;
+      }
 
-  if (direction % 2 === 0) {
-    console.log("right");
-    variantSlides[0].style.transform = "translateX(0)";
-    variantSlides[1].style.transform = "translateX(3%)";
-  } else {
-    console.log("left");
-    variantSlides[0].style.transform = "translateX(-200%)";
-    variantSlides[1].style.transform = "translateX(-203%)";
-    variantSlides[2].style.transform = "translateX(-200%)";
-    return;
-  }
-});
+      if (direction % 2 === 0) {
+        console.log("right");
+        variantSlides[0].style.transform = "translateX(0)";
+        variantSlides[1].style.transform = "translateX(3%)";
+      } else {
+        console.log("left");
+        variantSlides[0].style.transform = "translateX(-200%)";
+        variantSlides[1].style.transform = "translateX(-203%)";
+        variantSlides[2].style.transform = "translateX(-200%)";
+        return;
+      }
+    })
+  : "";
 
-variantSlides[2].addEventListener("swipe.progress", function (e) {
-  console.log("swiped begin", e.detail.direction, e);
-  const direction = e.detail.direction;
-  console.log(e.detail.x, e.detail.y, "dir");
+variantSlides[2] !== undefined
+  ? variantSlides[2].addEventListener("swipe.progress", function (e) {
+      console.log("swiped begin", e.detail.direction, e);
+      const direction = e.detail.direction;
+      console.log(e.detail.x, e.detail.y, "dir");
 
-  if (direction === null) {
-    return;
-  }
+      if (direction === null) {
+        return;
+      }
 
-  if (direction % 2 === 0) {
-    console.log("right here");
-    variantSlides[0].style.transform = "translateX(-103%)";
-    variantSlides[1].style.transform = "translateX(-100%)";
-    variantSlides[2].style.transform = "translateX(-97%)";
-  } else {
-    return;
-  }
-});
+      if (direction % 2 === 0) {
+        console.log("right here");
+        variantSlides[0].style.transform = "translateX(-103%)";
+        variantSlides[1].style.transform = "translateX(-100%)";
+        variantSlides[2].style.transform = "translateX(-97%)";
+      } else {
+        return;
+      }
+    })
+  : "";
 
 let slideTracker = 0;
-slides[slideTracker].style.opacity = 1;
+slides[slideTracker] !== undefined
+  ? (slides[slideTracker].style.opacity = 1)
+  : "";
 
 setInterval(function () {
   if (slideTracker < slides.length - 1) {
@@ -93,32 +150,38 @@ setInterval(function () {
     slides.forEach((item) => {
       item.style.opacity = 0;
     });
-    slides[slideTracker].style.opacity = 1;
+    slides[slideTracker] !== undefined
+      ? (slides[slideTracker].style.opacity = 1)
+      : "";
   } else {
     slideTracker = 0;
     slides.forEach((item) => {
       item.style.opacity = 0;
     });
-    slides[slideTracker].style.opacity = 1;
+    slides[slideTracker] !== undefined
+      ? (slides[slideTracker].style.opacity = 1)
+      : "";
   }
 }, 5000);
 
-headerLabel.addEventListener("click", function () {
-  if (!dropdownCheck.checked) {
-    setTimeout(function () {
-      dropdownItems.forEach((item) => {
-        item.style.opacity = 1;
-      });
-    }, 100);
+headerLabel !== undefined && headerLabel !== null
+  ? headerLabel.addEventListener("click", function () {
+      if (!dropdownCheck.checked) {
+        setTimeout(function () {
+          dropdownItems.forEach((item) => {
+            item.style.opacity = 1;
+          });
+        }, 100);
 
-    document.getElementsByTagName("body")[0].style.overflow = "hidden";
-  } else {
-    dropdownItems.forEach((item) => {
-      item.style.opacity = 0;
-      document.getElementsByTagName("body")[0].style.overflow = "scroll";
-    });
-  }
-});
+        document.getElementsByTagName("body")[0].style.overflow = "hidden";
+      } else {
+        dropdownItems.forEach((item) => {
+          item.style.opacity = 0;
+          document.getElementsByTagName("body")[0].style.overflow = "scroll";
+        });
+      }
+    })
+  : "";
 
 variantNavigatiors.forEach((item) => {
   item.addEventListener("click", function () {
@@ -129,5 +192,12 @@ variantNavigatiors.forEach((item) => {
       variantSlides[0].style.transform = "translateX(0)";
       variantSlides[1].style.transform = "translateX(10%)";
     }
+  });
+});
+
+console.log("variant btns", variantBtns);
+variantBtns.forEach((btn) => {
+  btn.addEventListener("click", function () {
+    console.log(btn.classList);
   });
 });
---
+++ src/js/variants.js
@@ -0,0 +1,126 @@
+import swipe from "pure-swipe-js";
+
+const flavourSlide = Array.from(
+  document.querySelectorAll(".flavour__slide")
+)[0];
+const flavourSlideNavs = Array.from(
+  document.querySelectorAll(".flavour__directions")
+);
+
+const flavours = Array.from(document.querySelectorAll(".flavour__container"));
+const flavoursSpan = Array.from(
+  document.querySelectorAll(".flavour__nav--span")
+);
+
+let flavourSlideTracker = 0;
+
+flavoursSpan.forEach((item) => {
+  flavoursSpan[flavourSlideTracker] === item
+    ? (item.style.background = "#ffcf3b")
+    : (item.style.background = "#fff");
+});
+
+flavourSlideNavs.forEach((item) => {
+  item.addEventListener("click", function (event) {
+    if (item.classList.contains("flavour__directions--next")) {
+      if (flavourSlideTracker < flavours.length - 1) {
+        flavourSlideTracker += 1;
+        console.log(flavourSlideTracker);
+
+        flavourSlide.style.transform = `translateX(-${flavourSlideTracker}00%)`;
+        flavours[flavourSlideTracker - 1] != null
+          ? (flavours[flavourSlideTracker - 1].style.opacity = "0")
+          : "";
+        flavours[flavourSlideTracker].style.opacity = "1";
+        flavours[flavourSlideTracker + 1] != null
+          ? (flavours[flavourSlideTracker + 1].style.opacity = "0")
+          : "";
+
+        flavoursSpan.forEach((item) => {
+          flavoursSpan[flavourSlideTracker] === item
+            ? (item.style.background = "#ffcf3b")
+            : (item.style.background = "#fff");
+        });
+      }
+      return;
+    }
+
+    if (flavourSlideTracker > 0) {
+      flavourSlideTracker -= 1;
+      console.log(flavourSlideTracker);
+
+      flavourSlideTracker !== 0
+        ? (flavourSlide.style.transform = `translateX(${flavourSlideTracker}00%)`)
+        : (flavourSlide.style.transform = `translateX(0)`);
+
+      flavours[flavourSlideTracker - 1] != null
+        ? (flavours[flavourSlideTracker - 1].style.opacity = "0")
+        : "";
+      flavours[flavourSlideTracker].style.opacity = "1";
+      flavours[flavourSlideTracker + 1] != null
+        ? (flavours[flavourSlideTracker + 1].style.opacity = "0")
+        : "";
+
+      flavoursSpan.forEach((item) => {
+        flavoursSpan[flavourSlideTracker] === item
+          ? (item.style.background = "#ffcf3b")
+          : (item.style.background = "#fff");
+      });
+    }
+  });
+});
+
+flavours.forEach((item) => {
+  item.addEventListener("swipe.progress", function (event) {
+    const direction = event.detail.direction;
+
+    if (direction === null) {
+      return;
+    }
+
+    if (direction % 2 === 0) {
+      if (flavourSlideTracker > 0) {
+        flavourSlideTracker -= 1;
+        console.log(flavourSlideTracker);
+
+        flavourSlideTracker !== 0
+          ? (flavourSlide.style.transform = `translateX(${flavourSlideTracker}00%)`)
+          : (flavourSlide.style.transform = `translateX(0)`);
+
+        flavours[flavourSlideTracker - 1] != null
+          ? (flavours[flavourSlideTracker - 1].style.opacity = "0")
+          : "";
+        flavours[flavourSlideTracker].style.opacity = "1";
+        flavours[flavourSlideTracker + 1] != null
+          ? (flavours[flavourSlideTracker + 1].style.opacity = "0")
+          : "";
+
+        flavoursSpan.forEach((item) => {
+          flavoursSpan[flavourSlideTracker] === item
+            ? (item.style.background = "#ffcf3b")
+            : (item.style.background = "#fff");
+        });
+      }
+    } else {
+      if (flavourSlideTracker < flavours.length - 1) {
+        flavourSlideTracker += 1;
+        console.log(flavourSlideTracker);
+
+        flavourSlide.style.transform = `translateX(-${flavourSlideTracker}00%)`;
+        flavours[flavourSlideTracker - 1] != null
+          ? (flavours[flavourSlideTracker - 1].style.opacity = "0")
+          : "";
+        flavours[flavourSlideTracker].style.opacity = "1";
+        flavours[flavourSlideTracker + 1] != null
+          ? (flavours[flavourSlideTracker + 1].style.opacity = "0")
+          : "";
+
+        flavoursSpan.forEach((item) => {
+          flavoursSpan[flavourSlideTracker] === item
+            ? (item.style.background = "#ffcf3b")
+            : (item.style.background = "#fff");
+        });
+      }
+    }
+  });
+});
