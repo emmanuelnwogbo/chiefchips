ea42f5c77e86ea2e45587da12b072fa43f8fe6f8
Emmanuel Nwogbo <nerdyemmanuel@gmail.com>
2020-08-14 09:14:35 +0100

    khbjcvhg

--- src/js/variants.js
+++ src/js/variants.js
@@ -127,11 +127,75 @@ flavours.forEach((item) => {
 
 const nutLabel = document.getElementById("nutrition-label");
 
-nutLabel !== null ? nutLabel.addEventListener("click", function (e) {
-  const flavourContainer = document.querySelector(".flavour__container");
-  const checkbox = document.getElementById("nutrition-check");
+nutLabel !== null
+  ? nutLabel.addEventListener("click", function (e) {
+      const flavourContainer = document.querySelector(".flavour__container");
+      const checkbox = document.getElementById("nutrition-check");
 
-  checkbox.checked
-    ? (flavourContainer.style.backgroundRepeat = `no-repeat`)
-    : (flavourContainer.style.backgroundRepeat = `repeat`);
-}) : '';
+      checkbox.checked
+        ? (flavourContainer.style.backgroundRepeat = `no-repeat`)
+        : (flavourContainer.style.backgroundRepeat = `repeat`);
+    })
+  : "";
+
+const flavourslider = document.querySelector(".flavour__slider");
+
+if (flavourslider !== null) {
+  const flavourDir = Array.from(
+    document.querySelectorAll(".flavour__directions")
+  );
+  const flavourBodies = Array.from(document.querySelectorAll(".flavour__body"));
+  const flavourPagination = Array.from(
+    document.querySelectorAll(".flavour__pagination--item")
+  );
+  let variantSlideTracker = 0;
+  let lengthOfSlider = flavourBodies.length;
+
+  function moveRight() {
+    if (variantSlideTracker < lengthOfSlider - 1) {
+      variantSlideTracker += 1;
+      flavourslider.style.transform = `translateX(-${variantSlideTracker}00%)`;
+      triggerFlavourPag();
+      showCurrentFlavBod();
+    }
+  }
+
+  function moveLeft() {
+    if (variantSlideTracker >= lengthOfSlider - 1 && variantSlideTracker > 0) {
+      variantSlideTracker -= 1;
+      flavourslider.style.transform = `translateX(-${variantSlideTracker}00%)`;
+      triggerFlavourPag();
+      showCurrentFlavBod();
+    }
+  }
+
+  flavourDir.forEach((item) => {
+    item.addEventListener("click", function (e) {
+      e.stopPropagation();
+      item.classList.contains("flavour__directions--next")
+        ? moveRight()
+        : moveLeft();
+    });
+  });
+
+  function triggerFlavourPag() {
+    flavourPagination.forEach((item) => {
+      item.classList.contains(`flavour__pagination-${variantSlideTracker}`)
+        ? (item.style.background = `rgb(255, 207, 59)`)
+        : (item.style.background = `#fff`);
+    });
+  }
+
+  function showCurrentFlavBod() {
+    flavourBodies.forEach((item) => {
+      item.style.opacity = "0";
+    });
+
+    flavourBodies[variantSlideTracker].style.opacity = "1";
+  }
+
+  flavourslider.addEventListener("swipe.progress", function (event) {
+    const direction = event.detail.direction;
+    direction % 2 === 0 ? moveLeft() : moveRight();
+  });
+}
